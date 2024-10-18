35fb7db606cf385bb8d9ef0bee62e211a075326a
Henry Shin <bg22915@bristol.ac.uk>
2024-10-18 03:50:34 +0100

    Comment out ewcViewSet and related imports for debugging
    • Temporarily commented out the ewcViewSet class and related imports (ewcSerializer and ewc model) for debugging purposes.
    
    • This change isolates potential issues in the viewset or model without affecting other parts of the codebase.

--- ewc/ewc_web/views.py
+++ ewc/ewc_web/views.py
@@ -1,8 +1,4 @@
 from django.shortcuts import render, redirect
-from rest_framework import viewsets
-from .serializers import ewcSerializer
-from.models import ewc
-
-class ewcViewSet(viewsets.ModelViewSet):
-    queryset = ewc.objects.all()
-    serializer_class = ewcSerializer
+# from rest_framework import viewsets
+# from .serializers import ewcSerializer
+# from.models import ewc
