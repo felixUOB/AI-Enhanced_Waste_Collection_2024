51b7a3a882f14b08ac0d59d77881355ddfb99345
Henry Shin <bg22915@bristol.ac.uk>
2024-10-21 21:18:44 +0100

    Fix import statements and restore views in urls.py
    Reinstated the import of views and routers in urls.py to enable correct API routing for the ewc_web app, ensuring functionality with Django Rest Framework components

--- ewc/ewc/urls.py
+++ ewc/ewc/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
-# from rest_framework import routers
-# from ewc_web import views
+from django.urls import path
+from rest_framework import routers
+from ewc_web import views
 
 # router = routers.DefaultRouter()
 # router.register(r'ewc_web', views.ewcViewSet, basename='ewc_web')
