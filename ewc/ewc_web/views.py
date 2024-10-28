76577703bd550007b72789afeb5b3720f914267b
Henry Shin <bg22915@bristol.ac.uk>
2024-10-28 02:43:15 +0000

    Add ViewSets for multiple models in ewc_web views.py
    Created ViewSet classes for UserProfile, CollectionPoint, JourneyMetric, and WastePrediction models.
    
    Each ViewSet includes a queryset for retrieving all objects and a corresponding serializer class.
    
    Added a basic home view rendering home.html.

--- ewc/ewc_web/views.py
+++ ewc/ewc_web/views.py
@@ -1,4 +1,24 @@
 from django.shortcuts import render, redirect
-# from rest_framework import viewsets
-# from .serializers import ewcSerializer
-# from.models import ewc
+from rest_framework import viewsets
+from .models import UserProfile, CollectionPoint, JourneyMetric, WastePrediction
+from .serializers import UserProfileSerializer, CollectionPointSerializer, JourneyMetricSerializer, WastePredictionSerializer
+
+def home(request):
+    return render(request, 'ewc_app/home.html')
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer
+
+class CollectionPointViewSet(viewsets.ModelViewSet):
+    queryset = CollectionPoint.objects.all()
+    serializer_class = CollectionPointSerializer
+
+class JourneyMetricViewSet(viewsets.ModelViewSet):
+    queryset = JourneyMetric.objects.all()
+    serializer_class = JourneyMetricSerializer
+
+class WastePredictionViewSet(viewsets.ModelViewSet):
+    queryset = WastePrediction.objects.all()
+    serializer_class = WastePredictionSerializer
+
