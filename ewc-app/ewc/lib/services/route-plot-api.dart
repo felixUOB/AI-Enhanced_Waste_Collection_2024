91d7f7f54c40dae46b8ec64ef94cf533e6484b5b
marek7j <114861445+marek7j@users.noreply.github.com>
2024-11-03 12:54:23 +0000

    Added comments to RouteService class in route-plot-api.dart

--- ewc-app/ewc/lib/services/route-plot-api.dart
+++ ewc-app/ewc/lib/services/route-plot-api.dart
@@ -1,21 +1,24 @@
+// Import necessary packages for route service and geolocation handling
 import 'package:open_route_service/open_route_service.dart';
 import 'package:latlong2/latlong.dart';
 
+// A service class to manage route fetching from OpenRouteService API
 class RouteService {
   final OpenRouteService client;
 
   //Constructor for initialization of the OpenRouteService client with an API key 
   RouteService(String apiKey) : client = OpenRouteService(apiKey: apiKey);
 
-  // Fetch the route between two coordinates
-      // Form Route between coordinates
+  // Form Route between coordinates returning a list of LatLng objects representing the route
   Future<List<LatLng>> getRoute(double startLat, double startLng, double endLat, double endLng) async {
+
+    // Fetch the route coordinates from OpenRouteService API using start and end coordinates
     final List<ORSCoordinate> routeCoordinates = await client.directionsRouteCoordsGet(
       startCoordinate: ORSCoordinate(latitude: startLat, longitude: startLng), 
       endCoordinate: ORSCoordinate(latitude: endLat, longitude: endLng),
     );
 
-  // Map route coordinates to a list of LatLng
+    // Convert the list of ORSCoordinate objects into LatLng objects representing the route to be display on a map
     return routeCoordinates.map((coordinate) => LatLng(coordinate.latitude, coordinate.longitude)).toList();
   }
 
