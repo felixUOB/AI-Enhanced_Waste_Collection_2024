bb63a2962262ebfe14fb5a989dd937c32cbc5e75
marek7j <114861445+marek7j@users.noreply.github.com>
2024-11-03 12:41:40 +0000

    Added comments for clarity and better documentation for MapPage and _MapPage classes in map.dart

--- ewc-app/ewc/lib/screens/map/map.dart
+++ ewc-app/ewc/lib/screens/map/map.dart
@@ -3,20 +3,21 @@ import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import '../../services/route-plot-api.dart';
-
+// MapPage is a stateful widget displaying a map and plotting a route
 class MapPage extends StatefulWidget{
   const MapPage({super.key});
 
+  // Creates and returns the private _MapPage state instance to manage the widget's state
   @override
   State<MapPage> createState() => _MapPage();
 }
 
-
+// Private State class for MapPage, manages state and map interactions
 class _MapPage extends State<MapPage> {
   final List<LatLng> _routePoints = [];
   late RouteService _routeService;
 
-  // ---- State initialisation ----
+  // State initialisation 
   @override
   void initState() {
     super.initState();
@@ -27,25 +28,29 @@ class _MapPage extends State<MapPage> {
   // This function loads .env and initializes RouteService asynchronously
   Future<void> _initializeEnvAndService() async {
     await dotenv.load(fileName: '.env'); // Load the .env file
-    // _routeService = RouteService(dotenv.env['API_KEY']!); // Initialize RouteService with API key
-    _routeService = RouteService(dotenv.env['API_KEY']!); // Initialize RouteService with API key
+    // Initialize RouteService with API key
+    _routeService = RouteService(dotenv.env['API_KEY']!);
     _fetchRoute();
   }
 
-  // ---- Fetching rout points, data ----
+  // Fetches rout data from the API
   Future<void> _fetchRoute() async {
     const startLat = 51.4553, startLng = -2.6050;
     const endLat = 51.4492, endLng = -2.5810;
 
+    // Get route points from the API and update _routePoints with the data
     final List<LatLng> route = await _routeService.getRoute(startLat, startLng, endLat, endLng);
 
     setState(() {
+      // Remove any existing points
       _routePoints.clear();
+      // Add new route points
       _routePoints.addAll(route);
     });
   }
 
 
+  // Builds the main UI for the map screen
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -56,6 +61,9 @@ class _MapPage extends State<MapPage> {
       body: content(),
     );
   }
+
+
+  // Widget that creates and displays map with initial configurations, route and markers
   Widget content() {
   return FlutterMap(
     options: MapOptions(
@@ -65,7 +73,10 @@ class _MapPage extends State<MapPage> {
         const InteractionOptions(flags: ~InteractiveFlag.doubleTapZoom),
     ),
     children: [
+      // Adds the OpenStreetMap tile layer to the map
       openStreetMapTileLayer,
+      
+      // Layer to display the route between route points fetched from API
       PolylineLayer(
         polylines: [
           Polyline(
@@ -76,6 +87,8 @@ class _MapPage extends State<MapPage> {
         ],
         
       ),
+      
+      // Layer to display a marker on the map
       MarkerLayer(markers: [
         Marker(
           point: LatLng(51.4516, -2.5810),
@@ -94,7 +107,7 @@ class _MapPage extends State<MapPage> {
 }
 }
 
-
+// Tile layer for OpenStreetMap tiles
 TileLayer get openStreetMapTileLayer => TileLayer(
   urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
   userAgentPackageName: 'dev.fleaflet.flutter_map.example',
