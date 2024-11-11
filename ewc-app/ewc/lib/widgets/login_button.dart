9b486d9e74a703a854605911bbac51e1c685a7f9
Henry Shin <bg22915@bristol.ac.uk>
2024-11-11 23:31:30 +0000

    feat(ui): update LoginButton widget to include onPressed callback
    - Added onPressed parameter to LoginButton for handling button actions
    - Enhanced flexibility by allowing custom actions to be passed to the login button

--- ewc-app/ewc/lib/widgets/login_button.dart
+++ ewc-app/ewc/lib/widgets/login_button.dart
@@ -3,29 +3,32 @@ import 'package:flutter/material.dart';
 
 class LoginButton extends StatelessWidget {
   final String text1;
+  final VoidCallback onPressed; // Added onPressed parameter
 
   const LoginButton({
     super.key,
-    required this.text1
-    });
+    required this.text1,
+    required this.onPressed, // Added onPressed to the constructor
+  });
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(25),
-      child: Container(
-      height: 50,
-      decoration: const BoxDecoration(
-        color: Colors.black,
-        borderRadius: BorderRadius.all(Radius.circular(8))
-      ),
-      child: Center(
-        child: Text(
-          " $text1",
-          style: constWhiteTextMedium,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.black,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8),
           ),
+          minimumSize: const Size(double.infinity, 50),
+        ),
+        onPressed: onPressed, // Set onPressed action
+        child: Text(
+          text1,
+          style: constWhiteTextMedium,
+        ),
       ),
-    )
     );
   }
 }
