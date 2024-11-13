95078e19045885b46b5b54486a8a5af9fe0b1619
Katie Pambakian <yj23812@bristol.ac.uk>
2024-11-13 10:31:02 +0000

    remove making a user page

--- ewc-app/ewc/lib/main.dart
+++ ewc-app/ewc/lib/main.dart
@@ -46,7 +46,7 @@ class myAppState extends State<App>{
     return MaterialApp(
       title: "EWC",
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home: ForgotPassword(),
       theme: lightTheme,
       darkTheme: darkTheme,
       themeMode: themeManager.themeMode
