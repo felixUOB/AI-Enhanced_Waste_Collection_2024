b529a746f4260824c69200d93a961b316c919381
felixUOB <zx23695@bristol.ac.uk>
2024-11-23 12:42:15 +0000

    cleaned map page

--- ewc-app/ewc/lib/screens/map/map.dart
+++ ewc-app/ewc/lib/screens/map/map.dart
@@ -1,4 +1,6 @@
-import 'package:ewc/imports/imports.dart';
+
+import 'package:flutter/material.dart';
+import 'package:ewc/widgets/theme_switch.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
