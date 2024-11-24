20ffcf3e1a2824954e98c89bd117d31209ca5256
felixUOB <zx23695@bristol.ac.uk>
2024-11-24 20:49:56 +0000

    removed duplicate widget

--- ewc-app/ewc/test/widget_test.dart
+++ ewc-app/ewc/test/widget_test.dart
@@ -5,7 +5,7 @@
 // // gestures. You can also use WidgetTester to find child widgets in the widget
 // // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:ewc/widgets/login_textfeild.dart';
+import 'package:ewc/widgets/login_textfield.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -19,7 +19,7 @@ void main() {
     await tester.pumpWidget(
       MaterialApp(
         home:Scaffold(
-          body: LoginTextfeild(
+          body: LoginTextfield(
             controller: controller, 
             hintText: 'Email', 
             obscured: false,
