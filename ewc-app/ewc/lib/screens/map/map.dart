628951cbd3fc9b0544d7a6a1a6f9cb33d23fa13c
em23081 <em23081@bristol.ac.uk>
2025-01-17 23:13:22 +0000

    Added basic recentre button

--- ewc-app/ewc/lib/screens/map/map.dart
+++ ewc-app/ewc/lib/screens/map/map.dart
@@ -6,6 +6,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import '../../services/route_plot_api.dart';
 import 'package:ewc/widgets/destination_marker_layer.dart';
 import 'package:ewc/widgets/route_polyline_layer.dart';
+import '../../widgets/recentre_button.dart';
 
 // MapPage is a stateful widget displaying a map and plotting a route
 class MapPage extends StatefulWidget {
@@ -20,6 +21,7 @@ class MapPage extends StatefulWidget {
 class _MapPage extends State<MapPage> {
   final List<LatLng> _routePoints = [];
   late RouteService _routeService;
+  final MapController mapController = MapController();
 
   // State initialisation
   @override
@@ -93,45 +95,49 @@ class _MapPage extends State<MapPage> {
       appBar: AppBar(
         //toolbarHeight: 75,
         title:
-            Text('RecycleNXT', style: Theme.of(context).textTheme.titleLarge),
+          Text('RecycleNXT', style: Theme.of(context).textTheme.titleLarge),
         //style: TextStyle(fontFamily: 'Questrial', fontSize: 32))
         actions: [
           SafeArea(
-              child: Container(
-                  // ignore: prefer_const_literals_to_create_immutables
-                  margin: EdgeInsets.only(right: 8.0),
-                  child: Column(
-                    children: [
-                      Padding(
-                          padding: const EdgeInsets.all(3),
-                          child:
-                              // ignore: prefer_const_constructors
-                              Align(
-                            alignment: Alignment.topRight,
-                            // ignore: prefer_const_constructors
-                            child: ThemeSwitch(),
-                          ))
-                    ],
-                  ))) // ignore: prefer_const_constructor
+            child: Container(
+              // ignore: prefer_const_literals_to_create_immutables
+              margin: EdgeInsets.only(right: 8.0),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(3),
+                    child:
+                        // ignore: prefer_const_constructors
+                        Align(
+                      alignment: Alignment.topRight,
+                      // ignore: prefer_const_constructors
+                      child: ThemeSwitch(),
+                    ))
+                ],
+              ))) // ignore: prefer_const_constructor
         ],
       ),
-      body: content(),
+      floatingActionButton: RecentreButton(onPressed: () {
+        mapController.move(LatLng(48.8584, 2.2945), 14);
+      }),
+      body: content(mapController),
     );
   }
 
   // Widget that creates and displays map with initial configurations, route and markers
-  Widget content() {
+  Widget content(MapController mapController) {
     return FlutterMap(
+      mapController: mapController,
       options: const MapOptions(
         initialCenter: LatLng(51.4492, -2.5879),
         minZoom: 1,
         maxZoom: 19,
         initialZoom: 14,
         interactionOptions:
-            InteractionOptions(
-                flags: ~InteractiveFlag.doubleTapZoom & // Disable double tap to zoom
-                       ~InteractiveFlag.rotate // Disable map rotation
-            ),
+        InteractionOptions(
+            flags: ~InteractiveFlag.doubleTapZoom & // Disable double tap to zoom
+            ~InteractiveFlag.rotate // Disable map rotation
+        ),
       ),
       children: [
         openStreetMapTileLayer, // Adds the OpenStreetMap tile layer to the map
@@ -143,7 +149,7 @@ class _MapPage extends State<MapPage> {
 
   // Tile layer for OpenStreetMap tiles
   TileLayer get openStreetMapTileLayer => TileLayer(
-        urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-        userAgentPackageName: 'dev.fleaflet.flutter_map.example',
-      );
+    urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+    userAgentPackageName: 'dev.fleaflet.flutter_map.example',
+  );
 }
