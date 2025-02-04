db3a0585001d854081bb84a70463bd5bbec34558
Katie Pambakian <yj23812@bristol.ac.uk>
2025-02-04 10:52:02 +0000

    basic logic

--- ewc_backend/ewc_core/model/model.py
+++ ewc_backend/ewc_core/model/model.py
@@ -5,8 +5,11 @@ from prophet import Prophet
 from prophet.plot import plot_plotly, plot_components_plotly
 import matplotlib.pyplot as plt
 
+# threshold value
+threshold = 8
+
 # load the data
-df = pd.read_csv('example.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/facebook/prophet/main/examples/example_wp_log_peyton_manning.csv')
 df.head()
 
 #fit the model
@@ -19,14 +22,27 @@ future.tail()
 
 # predict a value for each row
 forecast = m.predict(future)
+# yhat is the predicted value
 forecast[['ds', 'yhat','yhat_lower', 'yhat_upper']].tail()
 
 # plot the forecast
-fig1 = m.plot(forecast)
+# fig1 = m.plot(forecast)
 
-fig2 = m.plot_components(forecast)
+# fig2 = m.plot_components(forecast)
 
-plot_plotly(m, forecast)
+# plot_plotly(m, forecast)
 
-plot_components_plotly(m, forecast)
-plt.show()
+# plot_components_plotly(m, forecast)
+# plt.show()
+
+# idea:
+# predict the amount of waste that will be at each stop 
+# when it passes a threshold then it should be picked up
+
+# loop through the dates and the data
+for i in range(0, len(forecast.ds)):
+    date = forecast.ds[i]
+    data = forecast.yhat[i]
+    if (data > threshold):
+        print("pick up")
+    
