93cc2bf1367f46b0b4fcfe9f4a3df614dcf16920
felixUOB <zx23695@bristol.ac.uk>
2025-02-05 18:03:20 +0000

    basic deployment config files

--- ewc_backend/manage.py
+++ ewc_backend/manage.py
@@ -3,10 +3,11 @@
 import os
 import sys
 
+settings_module = 'ewc_admin.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'ewc_admin.settings'
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ewc_admin.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
