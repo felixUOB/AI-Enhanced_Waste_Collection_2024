b6603ea9a3f24d13ce7e9729e24089cbcbf3e21e
felixUOB <zx23695@bristol.ac.uk>
2025-02-13 00:55:18 +0000

    added versions to all installs to ensure consistency

--- ewc_backend/ewc_admin/settings.py
+++ ewc_backend/ewc_admin/settings.py
@@ -60,8 +60,7 @@ else:
     # Restrict to specific domains in production environment
     CORS_ORIGIN_ALLOW_ALL = False
     CORS_ALLOWED_ORIGINS = [
-        "https://yourdomain.com",
-        "https://anothertrusteddomain.com"
+        "https://devnest.software",
     ]
 
 
