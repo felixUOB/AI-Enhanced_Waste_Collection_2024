36ed0a232d0e19ab417c9cd9d6f17e65440602cb
Henry Shin <bg22915@bristol.ac.uk>
2025-02-20 23:01:52 +0000

    feat(routes): Add delete endpoint for stops
    - Imported `stops_delete_view` in `urls.py`
    
    - Created a new route `/stops/<int:pk>/delete/` to handle stop deletion

--- ewc_backend/ewc_admin/urls.py
+++ ewc_backend/ewc_admin/urls.py
@@ -27,7 +27,7 @@ from django.contrib.auth import views as auth_views
 # from ewc_core.ml_model.data import export_stopdata_csv
 # from ewc_core.ml_model.data import export_userdata_csv
 from ewc_core.ml_model.model import generate_csv_files
-from ewc_core.views import stops_list_view, stops_create_view, stops_edit_view
+from ewc_core.views import stops_list_view, stops_create_view, stops_edit_view, stops_delete_view
 
 
 # Router configuration for REST API endpoints
@@ -51,6 +51,7 @@ urlpatterns = [
     path('stops/', stops_list_view, name='stops_list'),                # List
     path('stops/new/', stops_create_view, name='stops_create'),        # Create
     path('stops/<int:pk>/edit/', stops_edit_view, name='stops_edit'),  # Edit
+    path('stops/<int:pk>/delete/', stops_delete_view, name='stops_delete'), # delete
 
 # -----------PASSWORD RESET ENDPOINTS--------------
     
