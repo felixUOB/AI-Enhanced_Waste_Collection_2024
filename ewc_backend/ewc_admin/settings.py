6d199b8ca541a61752944f99f3f2842e92e6ba70
felixUOB <zx23695@bristol.ac.uk>
2025-03-04 13:54:48 +0000

    fixed debug and CI push constraints

--- ewc_backend/ewc_admin/settings.py
+++ ewc_backend/ewc_admin/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = config('DEV_ENV', default=True, cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
