986c7612b91de3350ec975f2fed094d014d322be
Henry Shin <bg22915@bristol.ac.uk>
2025-03-11 13:09:31 +0000

    feat(metrics_service): streamline fetchAllRoutes with a map-based approach
    - Removed manual for-loop for constructing routeList
    
    - Now directly returns mapped JourneyRoute objects from JSON response

--- ewc_frontend/lib/services/metrics_service.dart
+++ ewc_frontend/lib/services/metrics_service.dart
@@ -3,23 +3,20 @@ import 'package:ewc/screens/metrics/route_data.dart';
 import 'package:ewc/service_locator.dart';
 import 'package:ewc/services/auth_service/auth_service.dart';
 
+
 class MetricsService {
   Future<List<JourneyRoute>> fetchAllRoutes() async {
-    final response =
         await getIt<AuthService>().makeAuthenticatedRequest('route_env_data/');
     // if the request is successful
+    final response = await getIt<AuthService>().makeAuthenticatedRequest('route_env_data/');
     if (response.statusCode == 200) {
       final data = jsonDecode(response.body) as List;
-      List<JourneyRoute> routeList = [];
-      for (var route in data) {
-        routeList.add(JourneyRoute(
-            distance: route['distance'],
-            mpg: route['mpg'],
-            date: route['date'],
-            filler: false));
-      }
-      return routeList;
-      // distance, mpg, date
+      return data.map((route) => JourneyRoute(
+        distance: route['distance'],
+        mpg: route['mpg'],
+        date: route['date'],
+        filler: false
+      )).toList();
     } else {
       throw Exception('Failed to load statistics data.');
     }
