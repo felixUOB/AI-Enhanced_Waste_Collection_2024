172356389f5503ac0c7a380b6b111d3d131620ff
felixUOB <zx23695@bristol.ac.uk>
2025-03-12 12:55:42 +0000

    documentation upload

--- ewc_frontend/test/mocks/mocks.mocks.dart
+++ ewc_frontend/test/mocks/mocks.mocks.dart
@@ -125,6 +125,17 @@ class MockAuthService extends _i1.Mock implements _i9.AuthService {
           as String);
 
   @override
+  String get rootUrl =>
+      (super.noSuchMethod(
+            Invocation.getter(#rootUrl),
+            returnValue: _i10.dummyValue<String>(
+              this,
+              Invocation.getter(#rootUrl),
+            ),
+          )
+          as String);
+
+  @override
   _i11.Future<void> initializeAuthService() =>
       (super.noSuchMethod(
             Invocation.method(#initializeAuthService, []),
@@ -160,14 +171,6 @@ class MockAuthService extends _i1.Mock implements _i9.AuthService {
             returnValueForMissingStub: _i11.Future<void>.value(),
           )
           as _i11.Future<void>);
-
-  @override
-  _i11.Future<bool> checkEmail(String? email) =>
-      (super.noSuchMethod(
-            Invocation.method(#checkEmail, [email]),
-            returnValue: _i11.Future<bool>.value(false),
-          )
-          as _i11.Future<bool>);
 
   @override
   _i11.Future<void> login(String? username, String? password) =>
