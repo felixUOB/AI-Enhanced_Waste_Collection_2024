8d57aeca7a787f4638e2cbe6dc832784760b3a62
marek7j <114861445+marek7j@users.noreply.github.com>
2025-03-16 19:58:57 +0000

    Redesigned the stops list page with a modern and more clear look; seperated listed stops into altenating shade blocks, buttons (adding hover animations)etc. and improved overall visual clarity; picked a more suitable map.

--- ewc_backend/static/js/stops_list.js
+++ ewc_backend/static/js/stops_list.js
@@ -1,8 +1,9 @@
 var map = L.map('map').setView([51.505, -0.09], 13);
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  maxZoom: 19,
-  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/">CARTO</a>',
+    subdomains: 'abcd',
+    maxZoom: 19
 }).addTo(map);
 var marker;
 
