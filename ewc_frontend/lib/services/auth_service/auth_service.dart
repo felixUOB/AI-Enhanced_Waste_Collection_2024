611ebc5a9c35c15778c13d5d291546df7103fe6b
felixUOB <zx23695@bristol.ac.uk>
2025-03-16 18:55:28 +0000

    bugs

--- ewc_frontend/lib/services/auth_service/auth_service.dart
+++ ewc_frontend/lib/services/auth_service/auth_service.dart
@@ -238,7 +238,7 @@ class AuthService {
 
 
 void launchPasswordReset() async {
-  final Uri resetUri = Uri.parse("http://127.0.0.1:8000/reset_password/");
+  final Uri resetUri = Uri.parse("$rootUrl/reset_password/");
 
     if (await canLaunchUrl(resetUri)) {
       await launchUrl(resetUri, mode: LaunchMode.externalApplication);
