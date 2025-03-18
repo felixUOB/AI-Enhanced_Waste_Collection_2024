31e44969053f60211fe9ef766bb74727e5cef253
felixUOB <zx23695@bristol.ac.uk>
2025-03-18 11:12:02 +0000

    latest

--- ewc_backend/ewc_admin/settings.py
+++ ewc_backend/ewc_admin/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'ewc_core',
-    'corsheaders',  # Added for CORS configuration
+    'corsheaders',
     'drf_yasg',
     'whitenoise.runserver_nostatic'
 ]
@@ -65,9 +65,6 @@ JAZZMIN_SETTINGS = {
     "search_model": ["auth.User", "auth.Group"],
     "user_avatar": None,
 
-    ############
-    # Top Menu #
-    ############
     "topmenu_links": [
         {"name": "Home",  "url": "admin:index", "permissions": ["auth.view_user"]},
         {"name": "Support", "url": "https://github.com/farridav/django-jazzmin/issues", "new_window": True},
@@ -75,57 +72,28 @@ JAZZMIN_SETTINGS = {
         {"app": "ewc_core"},
     ],
 
-    #############
-    # User Menu #
-    #############
-
     "usermenu_links": [
         {"name": "Support", "url": "https://github.com/farridav/django-jazzmin/issues", "new_window": True},
         {"model": "auth.user"}
     ],
-
-    #############
-    # Side Menu #
-    #############
 
     "show_sidebar": True,
     "navigation_expanded": True,
     "hide_apps": [],
     "hide_models": [],
 
-    # List of apps (and/or models) to base side menu ordering off of (does not need to contain all apps/models)
-    # "order_with_respect_to": ["auth", "books", "books.author", "books.book"],
-
-    # Custom links to append to app groups, keyed on app name
-    # "custom_links": {
-    #     "books": [{
-    #         "name": "Make Messages", 
-    #         "url": "make_messages", 
-    #         "icon": "fas fa-comments",
-    #         "permissions": ["books.view_book"]
-    #     }]
-    # },
-
-    # Custom icons for side menu apps/models See https://fontawesome.com/icons?d=gallery&m=free&v=5.0.0,5.0.1,5.0.10,5.0.11,5.0.12,5.0.13,5.0.2,5.0.3,5.0.4,5.0.5,5.0.6,5.0.7,5.0.8,5.0.9,5.1.0,5.1.1,5.2.0,5.3.0,5.3.1,5.4.0,5.4.1,5.4.2,5.13.0,5.12.0,5.11.2,5.11.1,5.10.0,5.9.0,5.8.2,5.8.1,5.7.2,5.7.1,5.7.0,5.6.3,5.5.0,5.4.2
-    # for the full list of 5.13.0 free icon classes
     "icons": {
         "auth": "fas fa-users-cog",
         "auth.user": "fas fa-user",
         "auth.Group": "fas fa-users",
     },
-    # Icons that are used when one is not manually specified
+
     "default_icon_parents": "fas fa-chevron-circle-right",
     "default_icon_children": "fas fa-circle",
 
-    #################
-    # Related Modal #
-    #################
     # Use modals instead of popups
     "related_modal_active": False,
 
-    #############
-    # UI Tweaks #
-    #############
     # Relative paths to custom CSS/JS scripts (must be present in static files)
     "custom_css": "admin/css/custom_admin.css",  # Add this line
     "custom_js": None,
@@ -133,10 +101,7 @@ JAZZMIN_SETTINGS = {
     "use_google_fonts_cdn": True,
     # Whether to show the UI customizer on the sidebar
     "show_ui_builder": True,
-
-    ###############
-    # Change view #
-    ###############
+    
     # Render out the change view as a single form, or in tabs, current options are
     # - single
     # - horizontal_tabs (default)
