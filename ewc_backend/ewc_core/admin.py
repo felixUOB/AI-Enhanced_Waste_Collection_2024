323f5877a3375cce27c1ee62accaac4e9893c9ae
felixUOB <zx23695@bristol.ac.uk>
2025-03-21 18:24:05 +0000

    forgot commenting

--- ewc_backend/ewc_core/admin.py
+++ ewc_backend/ewc_core/admin.py
@@ -1,22 +1,24 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import UserProfile, StopCollection, Stops, RouteEnvData
-from django.urls import path, reverse
-from django.shortcuts import redirect
+from django.urls import path
 from django.utils.html import format_html
 from .utils import generate_pdf
-from .views import stops_list_view, stops_create_view, stops_delete_view, stops_edit_view
+from .views import stops_list_view
 
 """
 This file registers the application's models with the Django admin interface,  
 allowing administrators to manage waste collection data through the Django admin panel.
 """
 
-# Register your models here
 admin.site.register(UserProfile)
 admin.site.register(StopCollection)
 
 class StopAdmin(admin.ModelAdmin):
+    """
+    This Model Admin class customizes the admin interface for the Stops model.
+    It links to custom html which adds a button to redirect to the Stops list view.
+    """
     change_list_template = "admin/stops_changelist.html"  # Use custom template for the admin list view
 
     def get_urls(self):
@@ -28,6 +30,10 @@ class StopAdmin(admin.ModelAdmin):
 admin.site.register(Stops, StopAdmin)
 
 class RouteEnvDataAdmin(admin.ModelAdmin):
+    """
+    This Model Admin class customizes the admin interface for the RouteEnvData model.
+    It adds a custom button to the admin interface to generate a PDF report.
+    """
     list_display = ['route_env_data_id', 'distance', 'mpg', 'date']  # Customize as needed
     change_list_template = "admin/route_env_data_changelist.html"  # Custom template
 
