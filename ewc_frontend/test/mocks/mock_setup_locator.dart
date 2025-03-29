b17f06cce8f69297554ca6cffd51a829e1395352
Henry Shin <bg22915@bristol.ac.uk>
2025-03-29 23:31:09 +0000

    refactor: Clean up

--- ewc_frontend/test/mocks/mock_setup_locator.dart
+++ ewc_frontend/test/mocks/mock_setup_locator.dart
@@ -2,10 +2,7 @@ import 'package:ewc/service_locator.dart';
 import 'package:ewc/services/stops_service.dart';
 import 'package:ewc/services/metrics_service.dart';
 
-/// A mock or stubbed version of setupLocator() that doesn't call RouteService.create().
-/// This way, we avoid the ".env" / "API_KEY" error.
 Future<void> mockSetupLocator() async {
-  // partial or minimal registration so the rest of the app won't crash.
 
   getIt.registerSingleton<StopsService>(StopsService());
   getIt.registerSingleton<MetricsService>(MetricsService());
