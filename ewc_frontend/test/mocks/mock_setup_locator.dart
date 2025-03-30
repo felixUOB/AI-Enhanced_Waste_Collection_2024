2e7dbe37c49d7649607cec465fb8d29374f4c068
Henry Shin <bg22915@bristol.ac.uk>
2025-03-30 15:29:44 +0100

    refactor: Clean up (remove mock_setup_locator)

--- ewc_frontend/test/mocks/mock_setup_locator.dart
+++
@@ -1,9 +0,0 @@
-import 'package:ewc/service_locator.dart';
-import 'package:ewc/services/stops_service.dart';
-import 'package:ewc/services/metrics_service.dart';
-
-Future<void> mockSetupLocator() async {
-
-  getIt.registerSingleton<StopsService>(StopsService());
-  getIt.registerSingleton<MetricsService>(MetricsService());
-}
