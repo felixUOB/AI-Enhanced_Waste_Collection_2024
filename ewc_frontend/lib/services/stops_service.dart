f84c7d4bc372fe37834346e672c0a92c3e8a8e50
felixUOB <zx23695@bristol.ac.uk>
2025-04-02 00:14:52 +0100

    stops bug fixed

--- ewc_frontend/lib/services/stops_service.dart
+++ ewc_frontend/lib/services/stops_service.dart
@@ -52,6 +52,13 @@ class StopsService {
                 description: point['description'])
             );
           }
+        } else {
+          stopsList.add(Stop(
+              id: point['stop_id'],
+              name: point['location_name'],
+              location: LatLng(point['latitude'], point['longitude']),
+              description: point['description'])
+          );
         }
       }
       return stopsList;
